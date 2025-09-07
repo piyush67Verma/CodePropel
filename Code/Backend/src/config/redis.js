e59922a84db06a8b8537a84457706aff34647a4d
Piyush Verma <piy06072003ace@gmail.com>
2025-09-07 23:57:41 +0530

    All Api updated

--- Code/Backend/src/config/redis.js
+++ Code/Backend/src/config/redis.js
@@ -4,8 +4,8 @@ const redisClient = redis.createClient({
     username: 'default',
     password: process.env.REDIS_PASSWORD,
     socket: {
-        host: 'redis-15761.c265.us-east-1-2.ec2.redns.redis-cloud.com',
-        port: 15761
+        host: 'redis-19391.crce182.ap-south-1-1.ec2.redns.redis-cloud.com',
+        port: 19391
     }
 })
 
